0237154f2e5dbd3c30446158de9568dcf8e47f94
ivarmani <93780319+ivarmani@users.noreply.github.com>
2024-05-14 15:55:27 +0000

    Update jackblack.py

--- jackblack.py
+++ jackblack.py
@@ -1,8 +1,14 @@
-from machine import Pin, SoftI2C, PWM
+from machine import Pin, SoftI2C
 from I2C_LCD import I2cLcd
 from time import sleep_ms
 import random
-#
+
+# skjárnir
+i2c = SoftI2C(scl=Pin(47), sda=Pin(48), freq=400000)
+lcd = I2cLcd(i2c, 0x27, 2, 16)
+lcd2 = I2cLcd(i2c, 0x21, 2, 16)
+
+# breytur og pinnar
 p1takki_green = Pin(13, Pin.IN, Pin.PULL_UP)
 p1ljos_green = Pin(14, Pin.OUT)
 p1takki_red = Pin(2, Pin.IN, Pin.PULL_UP)
@@ -13,11 +19,7 @@ p2ljos_green = Pin(12, Pin.OUT)
 p2takki_red = Pin(41, Pin.IN, Pin.PULL_UP)
 p2ljos_red = Pin(42, Pin.OUT)
 
-# Skjárinn nota I2C tengingu til að tala við ESP
-i2c = SoftI2C(scl=Pin(47), sda=Pin(48), freq=400000)
-lcd = I2cLcd(i2c, 0x27, 2, 16)
-lcd2 = I2cLcd(i2c, 0x21, 2, 16)
-
+#ljosin
 p1ljos1 = Pin(4, Pin.OUT)
 p1ljos2 = Pin(5, Pin.OUT)
 p1ljos3 = Pin(6, Pin.OUT)
@@ -28,16 +30,13 @@ p2ljos1 = Pin(9, Pin.OUT)
 p2ljos2 = Pin(15, Pin.OUT)
 p2ljos3 = Pin(16, Pin.OUT)
 p2ljos4 = Pin(17, Pin.OUT)
-p2ljos5 = Pin(18, Pin.OUT)
+p2ljos5 = Pin(3, Pin.OUT)
 
+# spila sort
 TIGULL = [0x00,0x04,0x0E,0x1F,0x0E,0x04,0x00,0x00]
-
-HJARTA =  [0x00,0x0A,0x1F,0x1F,0x0E,0x04,0x00,0x00]
-
+HJARTA = [0x00,0x0A,0x1F,0x1F,0x0E,0x04,0x00,0x00]
 SPADI = [0x00,0x04,0x0E,0x1F,0x1F,0x0A,0x04,0x0E]
-
 LAUF = [0x04,0x0E,0x15,0x15,0x15,0x0E,0x04,0x0E]
-
 
 tigulmerki = chr(0)
 hjartamerki = chr(1)
@@ -53,6 +52,12 @@ lcd2.custom_char(0, TIGULL) # chr(0)
 lcd2.custom_char(1, HJARTA)
 lcd2.custom_char(2, SPADI)
 lcd2.custom_char(3, LAUF)
+
+
+# Breytur fyrir stigin
+p1_stig = 0
+p2_stig = 0
+
 
 class Spil():
     def __init__(self, t, n):
@@ -77,9 +82,9 @@ class Spilastokkur():
     def __init__(self):
         self.stokkur = []
 
-    def smidaStokk(self): # Smíðar stokk
-        for y in range(4): # Skiptir spilum í tegund
-            if y == 0: #
+    def smidaStokk(self): 
+        for y in range(4): 
+            if y == 0: 
                 tegund = f"{hjartamerki}"
             elif y == 1:
                 tegund = f"{spadimerki}"
@@ -87,19 +92,26 @@ class Spilastokkur():
                 tegund = f"{tigulmerki}"
             elif y == 3:
                 tegund = f"{laufmerki}"
-            for x in range(1, 14): # Lætur inn 14 spil af hverri tegund í stokk
+            for x in range(1, 14): 
                 s1 = Spil(tegund, x)
                 self.stokkur.append(s1)
         
     
-    def gefaSpil(self): # Gefur spil út úr stokk
+    def gefaSpil(self): 
         randomtala=random.randint(0,len(self.stokkur) - 1)
         return self.stokkur.pop(randomtala)
 
+def birta_stig(player_stig, leds):
+    for index, led in enumerate(leds):
+        if index < player_stig:
+            led.on()
+        else:
+            led.off()
+
+Spila = Spilastokkur()
+Spila.smidaStokk()
+
 while True:
-    Spila = Spilastokkur()
-    Spila.smidaStokk()
-    
     p1listi=[]
     p2listi=[]
     
@@ -114,9 +126,35 @@ while True:
         lcd.putstr(''.join(p1listi))
         lcd.move_to(0, 1)
         lcd.putstr(p2listi[0])
-
         
         lcd2.move_to(0, 0)
         lcd2.putstr(''.join(p2listi))
         lcd2.move_to(0, 1)
         lcd2.putstr(p1listi[0])
+        
+        birta_stig(p1_stig, [p1ljos1, p1ljos2, p1ljos3, p1ljos4, p1ljos5])
+        birta_stig(p2_stig, [p2ljos1, p2ljos2, p2ljos3, p2ljos4, p2ljos5])
+        
+        if p1takki_green.value() == 0:
+            spil=f"{Spila.gefaSpil()}"
+            p1listi.append(spil)
+        elif p1takki_red.value() == 0:
+            break
+
+        if p2takki_green.value() == 0:
+            spil2=f"{Spila.gefaSpil()}"
+            p2listi.append(spil2)
+        elif p2takki_red.value() == 0:
+            break
+
+        p1_stig = sum([int(spil[1:]) for spil in p1listi if spil[1:].isdigit()])
+        p2_stig = sum([int(spil[1:]) for spil in p2listi if spil[1:].isdigit()])
+        
+        if p1_stig >= 5:
+            for led in [p1ljos1, p1ljos2, p1ljos3, p1ljos4, p1ljos5]:
+                led.on()
+            break
+        elif p2_stig >= 5:
+            for led in [p2ljos1, p2ljos2, p2ljos3, p2ljos4, p2ljos5]:
+                led.on()
+            break
